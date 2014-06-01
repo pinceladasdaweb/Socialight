f374efdbc627d3a7f0fb6565ab3338aecafd3e49
Pedro Rogério <pinceladasdaweb@hotmail.com>
2014-06-01 13:10:20 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        uglify: {
+            options: {
+                preserveComments: 'all'
+            },
+            src: {
+                files: {
+                    'build/socialight.min.js': ['src/socialight.js']
+                }
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', [ 'uglify' ]);
+};
