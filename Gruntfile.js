be51005c33cc368d87980dd0a1f6fa01e7462fee
Pedro Rogério <pinceladasdaweb@hotmail.com>
2014-06-07 17:41:11 -0300

    Update tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,42 @@
+var banner = '/* \n' +
+            '-------------------------------- \n' +
+            'Socialight \n' +
+            '-------------------------------- \n' +
+            '+ Get Social Network Share Counts with Vanilla JS \n' +
+            '+ https://github.com/pinceladasdaweb/Socialight \n' +
+            '+ version 0.0.3 \n' +
+            '+ Copyright 2014 Pedro Rogerio \n' +
+            '+ Licensed under the MIT license \n' +
+            '\n' +
+            '+ Documentation: https://github.com/pinceladasdaweb/Socialight \n' +
+            '*/ \n';
+
 module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
     grunt.initConfig({
+        concat: {
+            options: {
+                banner: banner,
+            },
+            dist: {
+                src: ['src/polyfill.js', 'src/promise.js', 'src/xhr.js', 'src/buffer.js', 'src/facebook.js', 'src/google-plus.js', 'src/linkedin.js', 'src/twitter.js', 'src/socialight.js'],
+                dest: 'build/socialight.js',
+            },
+        },
         uglify: {
             options: {
-                preserveComments: 'all'
+                banner: banner
             },
             src: {
                 files: {
-                    'build/socialight.min.js': [
-                        'src/promise.js',
-                        'src/socialight.js',
-                        'src/xhr.js',
-                        'src/twitter.js',
-                        'src/facebook.js',
-                        'src/linkedin.js',
-                        'src/buffer.js',
-                        'src/google-plus.js'
-                    ]
+                    'build/socialight.min.js': ['src/*']
                 }
             },
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', [ 'uglify' ]);
+    grunt.registerTask('default', [ 'concat', 'uglify' ]);
 };
