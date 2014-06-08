2f650e56aa203b48d3eebba356c5e41f8518a3d8
Pedro Rogério <pinceladasdaweb@hotmail.com>
2014-06-08 12:07:27 -0300

    update build versions

--- build/socialight.js
+++ build/socialight.js
@@ -4,7 +4,7 @@ Socialight
 -------------------------------- 
 + Get Social Network Share Counts with Vanilla JS 
 + https://github.com/pinceladasdaweb/Socialight 
-+ version 0.0.3 
++ version 0.0.4 
 + Copyright 2014 Pedro Rogerio 
 + Licensed under the MIT license 
 
@@ -209,6 +209,29 @@ var xhr = (function (window, document, undefined) {
                 resolve({
                     count: data.count,
                     name: "share-twitter"
+                });
+            });
+        },
+        promise: function (url) {
+            this.url = url;
+            return new Promise(this._request.bind(this));
+        }
+    };
+}(window));
+
+(function (window) {
+  "use strict";
+
+    window.Pinterest = function () {};
+
+    Pinterest.prototype = {
+        _request: function (resolve, reject) {
+            this.endpoint  = "https://api.pinterest.com/v1/urls/count.json?&url={url}".replace("{url}", this.url);
+
+            xhr.jsonp(this.endpoint, function (data) {
+                resolve({
+                    count: data.count,
+                    name: "share-pinterest"
                 });
             });
         },
