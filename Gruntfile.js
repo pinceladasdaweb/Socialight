e61ddc0e8a0d52c2b6c9e4a024b0eb94a2633e9d
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-11-01 13:56:31 -0200

    Update package dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,33 @@
-var banner = '/* \n' +
-            '-------------------------------- \n' +
-            'Socialight \n' +
-            '-------------------------------- \n' +
-            '+ Get Social Network Share Counts with Vanilla JS \n' +
-            '+ https://github.com/pinceladasdaweb/Socialight \n' +
-            '+ version 0.0.5 \n' +
-            '+ Copyright 2014 Pedro Rogerio \n' +
-            '+ Licensed under the MIT license \n' +
-            '\n' +
-            '+ Documentation: https://github.com/pinceladasdaweb/Socialight \n' +
-            '*/ \n';
-
 module.exports = function (grunt) {
-    pkg: grunt.file.readJSON('package.json'),
+    "use strict";
+
+    var pkg  = grunt.file.readJSON("package.json"),
+        date = new Date();
+
     grunt.initConfig({
-        concat: {
-            options: {
-                banner: banner,
-            },
-            dist: {
-                src: ['src/polyfill.js', 'src/promise.js', 'src/xhr.js', 'src/buffer.js', 'src/facebook.js', 'src/google-plus.js', 'src/linkedin.js', 'src/twitter.js', 'src/pinterest.js', 'src/socialight.js'],
-                dest: 'build/socialight.js',
-            },
+        meta: {
+          banner: '/*! ' + pkg.name + ' ' + pkg.version + ' | (c) ' + date.getFullYear() + ' ' + pkg.author + ' | ' + pkg.licenses[0].type + ' License */'
         },
         uglify: {
             options: {
-                banner: banner
+                banner: '<%= meta.banner %>\n'
             },
-            src: {
+            target: {
                 files: {
-                    'build/socialight.min.js': ['build/socialight.js']
+                    'build/socialight.min.js': ['src/socialight.js']
                 }
-            },
+            }
+        },
+        watch: {
+            js: {
+                files: ['src/socialight.js'],
+                tasks: ['uglify']
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', [ 'concat', 'uglify' ]);
+    grunt.registerTask('default', [ 'uglify' ]);
 };
